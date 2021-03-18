0c67456fe370a200a22e39184fa23082680684e7
DESKTOP-9JA4DHE\dmytr <dmytro.golovin.98@gmail.com>
2021-03-18 22:47:15 +0000

    Corrections of layout. Added footer. Added logo.

--- wee-shop-frontend/src/app/shared/shared.module.ts
+++ wee-shop-frontend/src/app/shared/shared.module.ts
@@ -5,16 +5,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { ProductScrollerComponent } from './components/product-scroller/product-scroller.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, LoadingSpinnerComponent, ProductScrollerComponent],
+  declarations: [NavBarComponent, LoadingSpinnerComponent, ProductScrollerComponent, FooterComponent],
   imports: [
     CommonModule,
     RouterModule,
     NgbModule
   ],
-  exports: [NavBarComponent, LoadingSpinnerComponent, ProductScrollerComponent]
+  exports: [NavBarComponent, LoadingSpinnerComponent, ProductScrollerComponent, FooterComponent]
 })
 export class SharedModule { }
