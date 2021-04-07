a5bb3c6e19a5d533cdbb783f909b7e520896bd14
DESKTOP-9JA4DHE\dmytr <dmytro.golovin.98@gmail.com>
2021-04-07 23:54:38 +0100

    Implemente login page (FE)

--- wee-shop-backoffice/src/app/pages/pages.module.ts
+++ wee-shop-backoffice/src/app/pages/pages.module.ts
@@ -6,16 +6,18 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
     SharedModule,
     FormsModule,
     NgbModule
-  ]
+  ],
+  exports: [LoginComponent]
 })
 export class PagesModule { }
